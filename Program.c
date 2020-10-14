d4f4150cd251aededa2934cf9676837caa7b63ae
unknown <denz8572@gmail.com>
2020-10-14 13:00:30 +0530

    commit done message

--- Program.c
+++ Program.c
@@ -3,7 +3,7 @@
 int main()
 {
     char name,country,skill;
-    printf("Welcome to Hacktoberfest 2020\n");
+    printf("Welcome to Hacktoberfest 2020\n");//Welcome message here
     printf("Enter your name : ");//Get User name
     scanf("%d",&name);
     printf("Enter your Country of Residance : ");//Get user country
@@ -11,7 +11,7 @@ int main()
     printf("Enter your Age : ");//Get user Age
     printf("%d",&age);
     printf("Rate your programmings skills from A-D (A being the highest and D being the lowest : ");//Get user skill level
-    scanf(" %c",&skill);
+    scanf(" %c",&skill);//reading the skill
     scanf("Thank you %c Happy Hacking",country);//Thank the user
 
     return 0;
